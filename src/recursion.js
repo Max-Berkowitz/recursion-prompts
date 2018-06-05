59d5ce19cd3cbed34a6ffb229c8620dfd57dc252
Max Berkowitz <maxl.berkowitz@gmail.com>
2018-06-04 19:51:05 -0700

    Added palindrome

--- src/recursion.js
+++ src/recursion.js
@@ -79,6 +79,10 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+	string = string.replace(/ /g, '').toLowerCase();
+	if(!string.length || string.length === 1) return true;
+	if(string[0] === string.slice(-1)) return palindrome(string.slice(1, -1));
+	return false;
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
