97993873607d4cff0b1d97b7928317551cff9935
Max Berkowitz <maxl.berkowitz@gmail.com>
2018-06-04 20:03:44 -0700

    Added modulo

--- src/recursion.js
+++ src/recursion.js
@@ -91,6 +91,11 @@ var palindrome = function(string) {
 // modulo(17,5) // 2
 // modulo(22,6) // 4
 var modulo = function(x, y) {
+	if(y === 0) return NaN;
+	if(x >= 0 && y < 0) y = -y;
+	if(x < 0 && y > 0) y = -y;
+	if((x >= 0 && x < y) || (x < 0 && x > y)) return x;
+	return modulo(x - y, y);
 };
 
 // 12. Write a function that multiplies two numbers without using the * operator or
