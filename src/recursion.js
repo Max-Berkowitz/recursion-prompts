93831bd415bbe32b6561c4517486d635239b794d
Max Berkowitz <maxl.berkowitz@gmail.com>
2018-06-08 12:21:37 -0700

    Added rMap

--- src/recursion.js
+++ src/recursion.js
@@ -169,13 +169,13 @@ var fizzBuzz = function(n) {
 // countOccurrence([2,7,4,4,1,4], 4) // 3
 // countOccurrence([2,'banana',4,4,1,'banana'], 'banana') // 2
 var countOccurrence = function(array, value) {
-  if(!array.length) return 0;
-  return +(array[0] === value) + countOccurrence(array.slice(1), value);
+  return !array.length ? 0 : +(array[0] === value) + countOccurrence(array.slice(1), value);
 };
 
 // 21. Write a recursive version of map.
 // rMap([1,2,3], timesTwo); // [2,4,6]
 var rMap = function(array, callback) {
+  return !array.length ? [] : [callback(array[0])].concat(rMap(array.slice(1), callback));
 };
 
 // 22. Write a function that counts the number of times a key occurs in an object.
